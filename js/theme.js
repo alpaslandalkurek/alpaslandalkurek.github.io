080c54653a105fcaab3700ddb6a2c82c2c7a5ea8
alpaslandalkurek <alp_dalkurek@hotmail.com>
2019-03-10 13:38:44 +0300

    production update

--- js/theme.js
+++ js/theme.js
@@ -180,8 +180,8 @@
      function preloader(){
         if ( $('.preloader').length ){ 
              $(window).load(function() {
-                $('.preloader').delay(500).fadeOut('slow');
-                $('body').delay(500).css({'overflow':'visible'});
+                $('.preloader').delay(1500).fadeOut('slow');
+                $('body').delay(1500).css({'overflow':'visible'});
             });
         } 
     }; 
